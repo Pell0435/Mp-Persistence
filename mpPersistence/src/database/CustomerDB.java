6b31947ea41dda751dd9a42829930c3d5e274cda
Pell0435 <10632141@ucn.dk>
2025-03-13 12:22:50 +0100

    gg

--- mpPersistence/src/database/CustomerDB.java
+++ mpPersistence/src/database/CustomerDB.java
@@ -1,38 +1,9 @@
 package database;
-import java.util.ArrayList;
-import java.util.List;
+
 
 import model.Customer;
 
 public class CustomerDB {
-
-	private static CustomerDB instance;
-	private ArrayList<Customer> customers;
 	
-	public static CustomerDB getInstance() {
-		if(instance == null) {
-			instance = new CustomerDB();
-		}
-		return instance;
-	}
-	
-	private CustomerDB() {
-		customers = new ArrayList<>();
-	}
-	
-	public void addCustomer (Customer c) throws IllegalArgumentException {
-		if(findByCustomerID(c.getCustomerID()) == null) {
-			Customer res = null;
-			for (int i = 0 ; i <customers.size() && res == null; i++) {
-				if(customers.get(i).getCustomerID().equalsIgnoreCase(CustomerID)) {
-					res = Customers.get(i);
-				}
-		}
-		return res;
-	}
-
-	public List<Customer> findAll() {
-		return new ArrayList<>(this.customers);
-	}
-
 }
+
